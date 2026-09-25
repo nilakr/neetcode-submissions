fc1bbe75dd8993671953c0a1d1cb9f5a64745055
nilakr <nila.krishnasami@gmail.com>
2026-09-25 00:33:47 -0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        mapS = defaultdict(int)
+        mapT = defaultdict(int)
+
+        for i in range(len(s)):
+            mapS[s[i]] += 1
+        
+        for i in range(len(t)):
+            mapT[t[i]] += 1
+
+        if mapS == mapT:
+            return True
+        else:
+            return False
+
