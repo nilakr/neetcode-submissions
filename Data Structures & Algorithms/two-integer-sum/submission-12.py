9d6830dcd4ae7fc93abbca1196ef6d953fd95d56
nilakr <nila.krishnasami@gmail.com>
2026-09-25 23:56:56 -0700

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            map[nums[i]] = i #maps each number in nums to its index
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in map:
+                if i != map[complement]:
+                    return [i, map[complement]]
+        
+
+        
